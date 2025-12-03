49e95603e02f49de190c0fa3cbf2390939364afc
nanoib <baydakov@nanocad.ru>
2025-12-03 13:44:23 +0300

    Family separated

--- SharpExpo.UI/obj/Debug/net8.0-windows/MainWindow.g.cs
+++ SharpExpo.UI/obj/Debug/net8.0-windows/MainWindow.g.cs
@@ -53,7 +53,7 @@ namespace SharpExpo.UI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SharpExpo.UI;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SharpExpo.UI;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
